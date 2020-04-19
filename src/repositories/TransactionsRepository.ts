ab77f1d0b7d32be7eb7f310706009d6644cc2508
Pedro Amaral <pedrotaem@gmail.com>
2020-04-19 01:45:22 -0300

    chore: Create and List Transactions

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,28 +1,44 @@
 import Transaction from '../models/Transaction';
+import Balance from '../models/Balance';
 
-interface Balance {
+interface BalanceDTO {
   income: number;
   outcome: number;
   total: number;
 }
-
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
 class TransactionsRepository {
   private transactions: Transaction[];
 
+  private balances: Balance;
+
   constructor() {
     this.transactions = [];
+    this.balances = {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    };
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
-  public getBalance(): Balance {
-    // TODO
+  public getBalance({ income, outcome, total }: BalanceDTO): Balance {
+    const balance = new Balance({ income, outcome, total });
+    this.balances = balance;
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
